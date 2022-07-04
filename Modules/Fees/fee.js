72999411f0847d2cd12e8c107746021db5f74660
Manish Pal <manish1999pal@gmail.com>
2022-07-04 10:32:09 +0530

    first commit

---
+++ Modules/Fees/fee.js
@@ -0,0 +1,47 @@
+
+const studentDetailCard = $('#studentDetails');
+const studentDetail =[
+
+    {
+        title:"Registraion No.",
+        value:"KV123"
+    },
+    {
+        title:"Full Name",
+        value:"Manish Pal"
+    },
+    {
+        title:"Roll No.",
+        value:"41"
+    },
+    {
+        title:"Standard",
+        value:"10-B"
+    },
+    {
+        title:"Father Name",
+        value:"Dinesh Pal"
+    },
+    {
+        title:"Contact",
+        value:"9876543210"
+    }
+]
+ 
+
+const addStudentDetail = ()=>{
+    studentDetailCard.empty();
+
+    studentDetail.map((val,ind)=>{
+        let deatilRow = `   <span class="mt-2">${val.title}</span>
+        <div class="form_group borderBlue">
+            <input class="form_input regNO" readonly  value="${val.value}" />
+          </div>`;
+        deatilRowElement = $.parseHTML( deatilRow );
+        studentDetailCard.append(deatilRowElement);
+    });
+
+
+
+
+}
