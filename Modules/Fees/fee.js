a25bd4dfadd2ebdf0e5d83b5249c6fadaca4695f
Manish Pal <manish1999pal@gmail.com>
2022-07-12 13:38:25 +0530

    fee part added Student Card Created

--- Modules/Fees/fee.js
+++ Modules/Fees/fee.js
@@ -1,61 +1,47 @@
 
 
-const studentDetail =[
-
-    {
-        title:"Registraion No.",
-        value:"KV123"
+const studentDetail = 
+{
+    student:{
+        fullName:"Manish Pal",
+        regNO:"KV123",
+        rollNo:"41",
+        std:"10-B",
+        father:"Dinesh Pal",
+        contact:"9876543210"
     },
-    {
-        title:"Full Name",
-        value:"Manish Pal"
-    },
-    {
-        title:"Roll No.",
-        value:"41"
-    },
-    {
-        title:"Standard",
-        value:"10-B"
-    },
-    {
-        title:"Father Name",
-        value:"Dinesh Pal"
-    },
-    {
-        title:"Contact",
-        value:"9876543210"
-    }
-]
- 
-
-const addStudentDetail = ()=>{
-    // studentDetailCard.empty();
-
-    // studentDetail.map((val,ind)=>{
-    //     let deatilRow = `   <span class="mt-2">${val.title}</span>
-    //     <div class="form_group borderBlue">
-    //         <input class="form_input regNO" readonly  value="${val.value}" />
-    //       </div>`;
-    //     deatilRowElement = $.parseHTML( deatilRow );
-    //     studentDetailCard.append(deatilRowElement);
-    // });
-
-    //get data first then 
-    $('#studentFind').hide();
-    
-    $('#studentDetails').show();
-
-
-
+    feeDtl:[
+        {
+            feeHeadName:"rahul",
+            date:'20/02/2022',
+            status:'not paid',
+            payAmount:230
+        },
+        {
+            feeHeadName:"rahul",
+            date:'20/02/2022',
+            status:'not paid',
+            payAmount:230
+        },
+        {
+            feeHeadName:"rahul",
+            date:'20/02/2022',
+            status:'not paid',
+            payAmount:230
+        }
+    ]
 }
 
-var stdDetail=[
-    {
-        name:"neeraj kumar",
-        regno:0001,
-        rollno:001,
-        standard:"9th",
-        
-    }
-]
+
+const addStudentDetail = () => {
+  
+    //get data first then 
+    $('#studentFind').hide();
+
+    $('#studentDetails').show();
+}
+
+
+function colorChange(){
+    document.getElementById("")
+}
