95ffdff8b3729d5cd6c0bf6cbf4c5fa9aa84a991
Manish Pal <manish1999pal@gmail.com>
2022-07-19 16:00:34 +0530

    new feature to be added

--- Modules/Fees/fee.js
+++ Modules/Fees/fee.js
@@ -14,19 +14,37 @@ const studentDetail =
         {
             feeHeadName:"rahul",
             date:'20/02/2022',
-            status:'not paid',
+            status:'unpaid',
             payAmount:230
         },
         {
             feeHeadName:"rahul",
             date:'20/02/2022',
-            status:'not paid',
+            status:'paid',
             payAmount:230
         },
         {
             feeHeadName:"rahul",
             date:'20/02/2022',
-            status:'not paid',
+            status:'underProcess',
+            payAmount:230
+        },
+        {
+            feeHeadName:"rahul",
+            date:'20/02/2022',
+            status:'unpaid',
+            payAmount:230
+        },
+        {
+            feeHeadName:"rahul",
+            date:'20/02/2022',
+            status:'paid',
+            payAmount:230
+        },
+        {
+            feeHeadName:"rahul",
+            date:'20/02/2022',
+            status:'underProcess',
             payAmount:230
         }
     ]
@@ -42,6 +60,36 @@ const addStudentDetail = () => {
 }
 
 
-function colorChange(){
-    document.getElementById("")
+
+const feeDropChange = () =>{
+   let selectId =  $('#feedropdown').val();
+   $('#feeRow').empty();
+
+   studentDetail.feeDtl.map((val,ind)=>{
+    if(val.status == selectId){
+        creatFeeRow(val);
+    }
+   })
+
 }
+
+const creatFeeRow = (feeDetail) =>{
+   
+    const rowDes = `<div class="row">
+        <div class="col-7">${feeDetail.feeHeadName}</div>
+        <div class="col-5 my_center_box">
+            <h4>₹ ${feeDetail.payAmount}</h4>
+        </div></div>`;
+    let feeRoot = document.createElement('div')
+
+    feeRoot.className = `card col-12 rounded p-1 mt-4 feeCard ${feeDetail.status}`
+
+    feeRoot.innerHTML = rowDes;
+    console.log(feeRoot)
+    document.getElementById('feeRow').append(feeRoot);
+   
+
+}
+
+//to create unpaid Fee List
+// feeDropChange();
